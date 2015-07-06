ee68d2148cd4c1c72fd494ba530a1782b21bf3a6
xonge <xonge007x@sina.com>
2015-07-06 08:14:55 +0800

    增加企业付款demo

--- PigCms/Lib/Action/Home/WxPayAction.class.php
+++ PigCms/Lib/Action/Home/WxPayAction.class.php
@@ -20,6 +20,7 @@ class WxPayAction {
 		$_payarr['amount'] = 1;
 		// o7F8auDz4SGsyIkBov3pWzA2C4jE
 //        o7F8auFB-s2WRNRcskQOI7Ap-Mn8
+//        fuck
 		$pay->mch_pay('o7F8auDz4SGsyIkBov3pWzA2C4jE', null, $_payarr);
 	}
 }
