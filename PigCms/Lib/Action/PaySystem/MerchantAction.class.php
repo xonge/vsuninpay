b4f543e1a05788145aa19f5e082ad292b64b8508
xonge <xonge007x@sina.com>
2015-07-25 09:59:15 +0800

    复制suninpay .net版

--- PigCms/Lib/Action/PaySystem/MerchantAction.class.php
+++ PigCms/Lib/Action/PaySystem/MerchantAction.class.php
@@ -1,5 +1,11 @@
 <?php
 class MerchantAction extends Action {
+
+    public function index() {
+        define('RES',THEME_PATH.'common');
+        $this->display();
+    }
+    // 商户信息管理
     public function infolist() {
         $this->display();
     }
