a7bd74de02d2c4f7884aae14c27098e4d4f2f623
xonge <xonge007x@sina.com>
2015-07-25 11:42:54 +0800

    复制suninpay .net版

--- PigCms/Lib/Action/PaySystem/IndexAction.class.php
+++ PigCms/Lib/Action/PaySystem/IndexAction.class.php
@@ -1,5 +1,10 @@
 <?php
 class IndexAction extends Action{
+
+    public function _initialize() {
+        define('RES',THEME_PATH.'common');
+    }
+
     public function index(){
         $this->display();
     }
